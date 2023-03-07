7799b9e82efeb664e2341d05ce5a540c4b5c7605
Vinayak Kukreja <vinakuk@amazon.com>
2023-03-07 13:38:55 -0500

    feat: add dependabot alert gh action
    Signed-off-by: Vinayak Kukreja <vinakuk@amazon.com>

--- .projenrc.js
+++ .projenrc.js
@@ -3,9 +3,7 @@ const project = new GitHubActionTypeScriptProject({
   defaultReleaseBranch: 'main',
   devDeps: ['projen-github-action-typescript'],
   name: 'cdk8s-action',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  deps: ['@octokit/rest'],
+  bundledDeps: ['@octokit/rest'],
 });
 project.synth();
